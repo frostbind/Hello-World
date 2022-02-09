502fde4411493fdce07f83834dc63c6ca20c6615
Alexander Farlinger <alexanderfarlinger@gmail.com>
2022-02-08 18:33:10 -0800

    init commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 
 
+
 /**
  * @author Alexander
  * @version 2022.01.5
@@ -8,7 +9,7 @@
 public class HelloWorld {
     /**
      * main method, drives program
-     * @param args
+     * @param args command line argument
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
